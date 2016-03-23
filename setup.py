256af345229134e0d2dc7ac857199b089a057bdc
Christophe Benz <christophe.benz@gmail.com>
2016-03-23 18:45:03 +0100

    Rename project

--- setup.py
+++ setup.py
@@ -6,23 +6,23 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='calculateur_impots',
+    name='calculette_impots',
     version='0.0.0.dev0',
 
     author='Christophe Benz',
     author_email='christophe.benz@data.gouv.fr',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
-        # 'License :: OSI Approved :: GNU Affero General Public License v3',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
         'Operating System :: POSIX',
         'Programming Language :: Python',
         'Topic :: Scientific/Engineering :: Information Analysis',
         ],
     description='Calculateur des impôts compilé en Python',
     keywords='calculateur impôts tax',
-    # license='http://www.fsf.org/licensing/licenses/agpl-3.0.html',
-    # url='',
+    license='http://www.fsf.org/licensing/licenses/agpl-3.0.html',
+    url='https://git.framasoft.org/openfisca/calculette-impots-python',
 
-    install_requires=['toolz'],
+    install_requires=['toolz >= 0.7.4'],
     packages=find_packages(),
     )
