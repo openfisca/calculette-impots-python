aadd32d159dc9a07e3a3b1e03a71566044d815e8
Christophe Benz <christophe.benz@gmail.com>
2016-03-30 15:50:29 +0200

    Add helper

--- calculette_impots/variables_definitions.py
+++ calculette_impots/variables_definitions.py
@@ -5,7 +5,7 @@ from . import loaders
 
 
 class VariablesDefinitions(object):
-    """Query `variables_definitions.json` file in a more convenient way."""
+    """Query `variables_definitions.json` file in a convenient way."""
 
     def __init__(self, definition_by_variable_name=None):
         self.definition_by_variable_name = definition_by_variable_name or loaders.load_variables_definitions()
@@ -26,8 +26,11 @@ class VariablesDefinitions(object):
         return self.definition_by_variable_name.get(variable_name, default).get('type')
 
     def is_calculee(self, variable_name, kind=None):
-        return self.get_type(variable_name) == 'variable_calculee' and \
-            (True if kind is None else self.definition_by_variable_name[variable_name].get(kind))
+        return self.is_type(variable_name, type='variable_calculee', kind=kind)
 
-    def is_saisie(self, variable_name):
-        return self.get_type(variable_name) == 'variable_saisie'
+    def is_saisie(self, variable_name, kind=None):
+        return self.is_type(variable_name, type='variable_saisie', kind=kind)
+
+    def is_type(self, variable_name, type, kind=None):
+        return self.get_type(variable_name) == type and \
+            (True if kind is None else self.definition_by_variable_name[variable_name].get(kind))
